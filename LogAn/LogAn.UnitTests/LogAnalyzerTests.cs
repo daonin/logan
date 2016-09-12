1652592aa38dc4df7d51d7e6d55f8358146cc64b
daonin <alex.koskin@gmail.com>
2016-09-12 19:40:38 +0300

    Starting ops

---
+++ LogAn/LogAn.UnitTests/LogAnalyzerTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace LogAn.UnitTests
+{
+    [TestFixture]
+    public class LogAnalyzerTests
+    {
+        [Test]
+        public void IsValidLogFileName_BadExtention_ReturnsFalse()
+        {
+            var logAnalyzer = new LogAnalyzer();
+
+            bool result = logAnalyzer.IsValidLogFileName("log.txt");
+
+            Assert.False(result);
+        }
+    }
+}
